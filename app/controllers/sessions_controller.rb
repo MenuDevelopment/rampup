ee3d2ac118cab687aaba3b847b4a453ca78449cb
adamtowers <hello@adamtowers.design>
2018-04-17 15:57:16 -0400

    Refactored login code.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:user][:username])
+    user = User.find_by(username: params[:username])
 
-    if user && user.try(:authenticate, params[:user][:password])
+    if user && user.try(:authenticate, params[:password])
       session[:user_id] = user.id
       redirect_to root_path
     else

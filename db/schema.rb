880b4847583e7e7007d42e83d72704b7ebcd66e4
adamtowers <hello@adamtowers.design>
2018-04-19 09:32:30 -0400

    Styled edit user form

--- db/schema.rb
+++ db/schema.rb
@@ -69,10 +69,6 @@ ActiveRecord::Schema.define(version: 20180419115832) do
     t.string "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo_file_name"
-    t.string "photo_content_type"
-    t.integer "photo_file_size"
-    t.datetime "photo_updated_at"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"

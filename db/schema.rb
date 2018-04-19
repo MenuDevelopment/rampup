9347888790d85e30ee8b8eb0f4d14f41bda3794e
Rani Shah <rani.shah@flatironschool.com>
2018-04-19 14:33:33 -0400

    Addworkoutbutton

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419132056) do
+ActiveRecord::Schema.define(version: 20180419132106) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -80,6 +80,10 @@ ActiveRecord::Schema.define(version: 20180419132056) do
     t.string "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
+    t.datetime "photo_updated_at"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"

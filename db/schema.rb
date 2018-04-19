f950d3b928117b828d603c6a8e500bc93c24d928
p-wong <patriciawong08@gmail.com>
2018-04-19 08:39:29 -0400

    Added default images for users in seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418171744) do
+ActiveRecord::Schema.define(version: 20180419115832) do
 
   create_table "exercise_workouts", force: :cascade do |t|
     t.integer "duration"
@@ -69,10 +69,15 @@ ActiveRecord::Schema.define(version: 20180418171744) do
     t.string "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
+    t.datetime "photo_updated_at"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "picture"
   end
 
   create_table "workouts", force: :cascade do |t|
